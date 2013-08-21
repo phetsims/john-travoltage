437d7238c89cc8b911ac0319dfd0f2a716d22b24
samreid <reids@colorado.edu>
2013-08-21 12:37:36 -0600

    Removed unused vars

--- js/model/Box2DModel.js
+++ js/model/Box2DModel.js
@@ -17,12 +17,8 @@ define( function( require ) {
     b2BodyDef = Box2D.Dynamics.b2BodyDef,
     b2Body = Box2D.Dynamics.b2Body,
     b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
-    b2Fixture = Box2D.Dynamics.b2Fixture,
     b2World = Box2D.Dynamics.b2World,
-    b2MassData = Box2D.Collision.Shapes.b2MassData,
-    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
-    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
-    b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
 
   function Box2DModel( verts, forceLines ) {
     PropertySet.call( this, {
