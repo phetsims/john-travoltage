f5e4e630636db99693e78bd2283d60f6d49fad15
samreid <reids@colorado.edu>
2013-09-20 08:53:03 -0600

    cleaned up main imports

--- js/john-travoltage-main.js
+++ js/john-travoltage-main.js
@@ -3,20 +3,15 @@
 /**
  * Main entry point for the sim.
  *
- * @author Chris Malley (PixelZoom, Inc.)
  * @author Sam Reid (PhET Interactive Simulations)
  */
 require( [
   'JOIST/SimLauncher',
-  'SCENERY/nodes/Text',
-  'SCENERY/nodes/Rectangle',
   'Strings',
   'JOIST/Sim',
-  'model/JohnTravoltageModel',
-  'view/JohnTravoltageView',
   'JohnTravoltageImages',
   'JohnTravoltageScreen'
-], function( SimLauncher, Text, Rectangle, Strings, Sim, JohnTravoltageModel, JohnTravoltageView, JohnTravoltageImages, JohnTravoltageScreen ) {
+], function( SimLauncher, Strings, Sim, JohnTravoltageImages, JohnTravoltageScreen ) {
   'use strict';
 
   //Create and start the sim
