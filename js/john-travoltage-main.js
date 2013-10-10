c6f417ea6f47247865210af43cd6b08d26f5ffc3
samreid <reids@colorado.edu>
2013-10-10 15:48:40 -0600

    added credits, see #40

--- js/john-travoltage-main.js
+++ js/john-travoltage-main.js
@@ -19,8 +19,19 @@ require( [
   newChild.style.display = 'none';
   document.body.appendChild( newChild );
 
+  var simOptions = {
+    credits: 'PhET Development Team -\n' +
+             'Lead Design: Noah Podolefsky, Carl Wieman, Sam Reid\n' +
+             'Software Development: Sam Reid\n' +
+             'Design: Ariel Paul, Kathy Perkins, Trish Loeblein, Sharon Simon-Tov\n' +
+             'Interviews: Ariel Paul, Wendy Adams\n',
+    thanks: 'Thanks -\n' +
+            'Thanks to Mobile Learner Labs for working with the PhET development team\n' +
+            'to convert this simulation to HTML5.'
+  };
+
   //Create and start the sim
   SimLauncher.launch( function() {
-    new Sim( title, [new JohnTravoltageScreen()] ).start();
+    new Sim( title, [new JohnTravoltageScreen()], simOptions ).start();
   } );
 } );
