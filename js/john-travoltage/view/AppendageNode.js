74477ff34cb3edbec298d714e8d3d92ab5ca00ca
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-02-09 11:21:35 -0700

    fix incorrect setting of dragging state, see #189

--- js/john-travoltage/view/AppendageNode.js
+++ js/john-travoltage/view/AppendageNode.js
@@ -218,7 +218,7 @@ define( function( require ) {
         self.dragging = true;
       },
       blur: function( event ) {
-        self.dragging = 'false';
+        self.dragging = false;
       }
     } );
 
