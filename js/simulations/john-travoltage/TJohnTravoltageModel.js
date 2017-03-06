290a5314228afccc8560ee592d0b58f78a707a81
samreid <reids@colorado.edu>
2017-03-06 14:09:01 -0700

    Removed completed TODO, see https://github.com/phetsims/phet-io/issues/44

--- js/simulations/john-travoltage/TJohnTravoltageModel.js
+++ js/simulations/john-travoltage/TJohnTravoltageModel.js
@@ -14,8 +14,6 @@ define( function( require ) {
   var phetioInherit = require( 'PHET_IO/phetioInherit' );
   var TObject = require( 'PHET_IO/types/TObject' );
 
-  // TODO: this file is likely where we will deal with serializing the particles
-
   var TJohnTravoltageModel = function( instance, phetioID ) {
     assertInstanceOf( instance, phet.johnTravoltage.JohnTravoltageModel );
     TObject.call( this, instance, phetioID );
