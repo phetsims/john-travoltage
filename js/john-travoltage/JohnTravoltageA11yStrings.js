f8eb5182be1a01f36038d08711ad6db7f8a5ec94
Bose <bose@engr2-2-81-46-dhcp.int.colorado.edu>
2017-04-21 11:24:07 -0600

    move space, see #230

--- js/john-travoltage/JohnTravoltageA11yStrings.js
+++ js/john-travoltage/JohnTravoltageA11yStrings.js
@@ -15,9 +15,9 @@ define( function( require ) {
   var JohnTravoltageA11yStrings = {
 
     // summary information
-    sceneDescriptionString: '{1}John\'s hand is {0}, and he is ready to swing his leg to rub his foot on the rug.',
+    sceneDescriptionString: '{1} John\'s hand is {0}, and he is ready to swing his leg to rub his foot on the rug.',
     electronsDescriptionSingleString: 'John has 1 charge on his body.',
-    electronsDescriptionMultipleString: 'John has {0} charges on his body. ',
+    electronsDescriptionMultipleString: 'John has {0} charges on his body.',
     positionTemplateString: 'Position {0}: {1}',
     electronsTotalString: 'Electrons on body: {0}',
     electronsTotalAfterDischargeString: 'Electrons on body: Discharge occurred. Electrons decreased from {1} to {0}.',
