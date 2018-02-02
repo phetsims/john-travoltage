3c4060002cedb85cd3e1498bfea90f12cf485288
Jesse Greenberg <jesse.greenberg@colorado.edu>
2018-02-01 18:22:06 -0700

    fixes #268

--- js/john-travoltage/model/Arm.js
+++ js/john-travoltage/model/Arm.js
@@ -14,6 +14,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var johnTravoltage = require( 'JOHN_TRAVOLTAGE/johnTravoltage' );
   var Vector2 = require( 'DOT/Vector2' );
+  var Range = require( 'DOT/Range' );
 
   /**
    * @param {Tandem} tandem
@@ -24,7 +25,13 @@ define( function( require ) {
     // position determined empirically with DebugUtils
     var pivotPoint = new Vector2( 423.6179673321235, 229.84969476984 );
 
-    Appendage.call( this, pivotPoint, tandem );
+    Appendage.call( this, pivotPoint, tandem, {
+
+      // with mouse interaction, arm rotates from -pi to pi. With the keyboard, there are specific locations where
+      // '0' is at the doorknob, and so the value goes from -pi + 0.41 to pi + 0.41. So this includes the full
+      // range, see https://github.com/phetsims/john-travoltage/issues/268
+      range: new Range( -Math.PI, Math.PI + 0.41 )
+    } );
 
     // Exact finger location sampled using DebugUtils.js
     var finger = new Vector2( 534.3076703633706, 206.63766358806117 );
