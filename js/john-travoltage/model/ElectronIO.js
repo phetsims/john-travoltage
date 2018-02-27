43baf7c1b774323ee3b50b7dbfa4a0b4ad26dd36
Chris Malley <cmalley@pixelzoom.com>
2018-02-27 12:48:59 -0700

    group "phet-io modules" #1307

--- js/john-travoltage/model/ElectronIO.js
+++ js/john-travoltage/model/ElectronIO.js
@@ -10,8 +10,10 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var johnTravoltage = require( 'JOHN_TRAVOLTAGE/johnTravoltage' );
+  
+  // phet-io modules
+  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var ObjectIO = require( 'ifphetio!PHET_IO/types/ObjectIO' );
   var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
 
