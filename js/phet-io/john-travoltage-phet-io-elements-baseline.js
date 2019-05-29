d39ee6b9cd96465152730f793528ced10ffeac59
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:54 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/john-travoltage-phet-io-elements-baseline.js
+++ js/phet-io/john-travoltage-phet-io-elements-baseline.js
@@ -1731,7 +1731,7 @@ window.phet.phetio.phetioElementsBaseline =
     "johnTravoltage.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
