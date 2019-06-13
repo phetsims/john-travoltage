d6b12ca895829f48f44e1b51203a065ba5e170d3
zepumph <michael.kauzmann@colorado.edu>
2019-06-13 13:42:51 -0800

    update baselines and remove redundant GQ overrides, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/john-travoltage-phet-io-elements-baseline.js
+++ js/phet-io/john-travoltage-phet-io-elements-baseline.js
@@ -1544,7 +1544,7 @@ window.phet.phetio.phetioElementsBaseline =
     "johnTravoltage.navigationBar.phetButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
