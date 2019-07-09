9a1738a332601caddb5784c4f6c9d148f7e6d9a3
Saurabh Totey <SaurabhTotey@gmail.com>
2019-07-09 09:54:01 -0600

    Migrated ArmPositionSoundGenerator to ES6 for superconstructor

--- js/john-travoltage/view/ArmPositionSoundGenerator.js
+++ js/john-travoltage/view/ArmPositionSoundGenerator.js
@@ -6,105 +6,104 @@
  *
  * @author John Blanco
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var inherit = require( 'PHET_CORE/inherit' );
-  var johnTravoltage = require( 'JOHN_TRAVOLTAGE/johnTravoltage' );
-  var SoundClip = require( 'TAMBO/sound-generators/SoundClip' );
-  var SoundGenerator = require( 'TAMBO/sound-generators/SoundGenerator' );
+  const johnTravoltage = require( 'JOHN_TRAVOLTAGE/johnTravoltage' );
+  const SoundClip = require( 'TAMBO/sound-generators/SoundClip' );
+  const SoundGenerator = require( 'TAMBO/sound-generators/SoundGenerator' );
 
   // sounds
-  var armPosition01Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-001.mp3' );
-  var armPosition02Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-002.mp3' );
-  var armPosition03Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-003.mp3' );
-  var armPosition04Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-004.mp3' );
-  var armPosition05Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-005.mp3' );
-  var armPosition06Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-006.mp3' );
+  const armPosition01Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-001.mp3' );
+  const armPosition02Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-002.mp3' );
+  const armPosition03Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-003.mp3' );
+  const armPosition04Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-004.mp3' );
+  const armPosition05Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-005.mp3' );
+  const armPosition06Audio = require( 'sound!JOHN_TRAVOLTAGE/arm-position-006.mp3' );
 
   // constants
-  var NUM_SOUND_POSITIONS = 32;
-  var MAX_SOUNDS_PER_ITERATION = 3; // empirically determined
-  var ANGLE_OF_HIGHEST_PITCH = 0.45; // this is when the finger is closest to the door knob
+  const NUM_SOUND_POSITIONS = 32;
+  const MAX_SOUNDS_PER_ITERATION = 3; // empirically determined
+  const ANGLE_OF_HIGHEST_PITCH = 0.45; // this is when the finger is closest to the door knob
 
-  /**
-   * @constructor
-   * {Property.<number>} armAngleProperty - angle, in radians, of the arm position
-   * {Object} [options]
-   */
-  function ArmPositionSoundGenerator( armAngleProperty, options ) {
+  class ArmPositionSoundGenerator extends SoundGenerator {
 
-    var self = this;
-    SoundGenerator.call( this, options );
+    /**
+     * @constructor
+     * {Property.<number>} armAngleProperty - angle, in radians, of the arm position
+     * {Object} [options]
+     */
+    constructor( armAngleProperty, options ) {
 
-    var ratchetSounds = [
-      new SoundClip( armPosition01Audio ),
-      new SoundClip( armPosition02Audio ),
-      new SoundClip( armPosition03Audio ),
-      new SoundClip( armPosition04Audio ),
-      new SoundClip( armPosition05Audio ),
-      new SoundClip( armPosition06Audio )
-    ];
+      super( options );
 
-    // define a helper function to play a random ratchet sound
-    var previousRatchetSoundIndex = -1;
+      const ratchetSounds = [
+        new SoundClip( armPosition01Audio ),
+        new SoundClip( armPosition02Audio ),
+        new SoundClip( armPosition03Audio ),
+        new SoundClip( armPosition04Audio ),
+        new SoundClip( armPosition05Audio ),
+        new SoundClip( armPosition06Audio )
+      ];
 
-    function playRandomRatchetSound( playbackRate ) {
-      do {
-        var ratchetSoundIndex = Math.floor( phet.joist.random.nextDouble() * ratchetSounds.length );
-      } while ( ratchetSoundIndex === previousRatchetSoundIndex );
-      ratchetSounds[ ratchetSoundIndex ].setPlaybackRate( playbackRate, 1E-30 );
-      ratchetSounds[ ratchetSoundIndex ].play();
-      previousRatchetSoundIndex = ratchetSoundIndex;
+      // define a helper function to play a random ratchet sound
+      let previousRatchetSoundIndex = -1;
+
+      const playRandomRatchetSound = playbackRate => {
+        let ratchetSoundIndex;
+        do {
+          ratchetSoundIndex = Math.floor( phet.joist.random.nextDouble() * ratchetSounds.length );
+        } while ( ratchetSoundIndex === previousRatchetSoundIndex );
+        ratchetSounds[ ratchetSoundIndex ].setPlaybackRate( playbackRate, 1E-30 );
+        ratchetSounds[ ratchetSoundIndex ].play();
+        previousRatchetSoundIndex = ratchetSoundIndex;
+      };
+
+      // connect up the sound generators
+      ratchetSounds.forEach( ratchetSound => { ratchetSound.connect( this.masterGainNode ); } );
+
+      // create the array containing the 'sound points', which are the arm position where a sound sample is played
+      const soundPositions = [];
+      _.times( NUM_SOUND_POSITIONS, index => {
+        soundPositions.push( -Math.PI + index / NUM_SOUND_POSITIONS * Math.PI * 2 );
+      } );
+      const binSize = 2 * Math.PI / NUM_SOUND_POSITIONS;
+
+      // monitor the arm property, adjusting and playing sounds as appropriate
+      let previousAngle = null;
+      armAngleProperty.lazyLink( angle => {
+
+        let numClickSoundsToPlay = 0;
+
+        // determine if the new angle crossed over one or more 'sound points' since the last angle
+        if ( this.fullyEnabledProperty.value && previousAngle !== null ) {
+          numClickSoundsToPlay = Math.abs( Math.floor( previousAngle / binSize ) -
+                                           Math.floor( angle / binSize ) );
+          // TODO: This is a temporary workaround for an issue where the angle seems to switch from + to - at an odd place
+          if ( numClickSoundsToPlay > NUM_SOUND_POSITIONS / 2 ) {
+            numClickSoundsToPlay = 0;
+          }
+        }
+
+        // play the sounds that indicate the motion, but limit it in the case of really large changes
+        let angularDistanceFromKnob = Math.abs( ANGLE_OF_HIGHEST_PITCH - angle );
+        if ( angularDistanceFromKnob > Math.PI ) {
+          angularDistanceFromKnob = Math.PI - angularDistanceFromKnob % Math.PI;
+        }
+        const playbackRate = 0.75 + 0.75 * ( 1 - angularDistanceFromKnob / Math.PI );
+        _.times( Math.min( numClickSoundsToPlay, MAX_SOUNDS_PER_ITERATION ), () => {
+          playRandomRatchetSound( playbackRate );
+        } );
+
+        // save the angle for the next time through
+        previousAngle = angle;
+      } );
     }
 
-    // connect up the sound generators
-    ratchetSounds.forEach( function( ratchetSound ) {
-      ratchetSound.connect( self.masterGainNode );
-    } );
-
-    // create the array containing the 'sound points', which are the arm position where a sound sample is played
-    var soundPositions = [];
-    _.times( NUM_SOUND_POSITIONS, function( index ) {
-      soundPositions.push( -Math.PI + index / NUM_SOUND_POSITIONS * Math.PI * 2 );
-    } );
-    var binSize = 2 * Math.PI / NUM_SOUND_POSITIONS;
-
-    // monitor the arm property, adjusting and playing sounds as appropriate
-    var previousAngle = null;
-    armAngleProperty.lazyLink( function( angle ) {
-
-      var numClickSoundsToPlay = 0;
-
-      // determine if the new angle crossed over one or more 'sound points' since the last angle
-      if ( self.fullyEnabledProperty.get() && previousAngle !== null ) {
-        numClickSoundsToPlay = Math.abs( Math.floor( previousAngle / binSize ) -
-                                         Math.floor( angle / binSize ) );
-        // TODO: This is a temporary workaround for an issue where the angle seems to switch from + to - at an odd place
-        if ( numClickSoundsToPlay > NUM_SOUND_POSITIONS / 2 ) {
-          numClickSoundsToPlay = 0;
-        }
-      }
-
-      // play the sounds that indicate the motion, but limit it in the case of really large changes
-      var angularDistanceFromKnob = Math.abs( ANGLE_OF_HIGHEST_PITCH - angle );
-      if ( angularDistanceFromKnob > Math.PI ) {
-        angularDistanceFromKnob = Math.PI - angularDistanceFromKnob % Math.PI;
-      }
-      var playbackRate = 0.75 + 0.75 * ( 1 - angularDistanceFromKnob / Math.PI );
-      _.times( Math.min( numClickSoundsToPlay, MAX_SOUNDS_PER_ITERATION ), function() {
-        playRandomRatchetSound( playbackRate );
-      } );
-
-      // save the angle for the next time through
-      previousAngle = angle;
-    } );
   }
 
   johnTravoltage.register( 'ArmPositionSoundGenerator', ArmPositionSoundGenerator );
-
-  inherit( SoundGenerator, ArmPositionSoundGenerator );
 
   return ArmPositionSoundGenerator;
 } );
