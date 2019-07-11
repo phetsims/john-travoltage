a0e9ad2d9bfcb98b49c165cb64c7f3942e15c1c6
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:54:08 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/john-travoltage/model/Appendage.js
+++ js/john-travoltage/model/Appendage.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2018, University of Colorado Boulder
+// Copyright 2017-2019, University of Colorado Boulder
 
 /**
  * A base type for appedages in this sim. Extended by Arm.js and Leg.js.  Appendages have a pivot point, and an
