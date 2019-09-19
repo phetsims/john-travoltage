388f3d2a7a40b7115b291d5f0e5e4c120cf1360b
phet-dev <phetadmin@gmail.com>
2019-09-19 03:16:41 -0600

    update copyright dates from daily grunt work

--- js/john-travoltage/view/vibrationController.js
+++ js/john-travoltage/view/vibrationController.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2019, University of Colorado Boulder
 
 /**
  * Controls vibration in john-travoltage through use of tappi's vibrationManager. There are three paradigms
