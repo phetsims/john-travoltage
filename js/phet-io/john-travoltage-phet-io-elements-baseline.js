54001287691f5c554c0db1caabeaa6eb1b813474
zepumph <michael.kauzmann@colorado.edu>
2019-11-20 16:02:54 -0900

    rename PhetioSingleton -> PhetioCapsule, https://github.com/phetsims/phet-io/issues/1567

--- js/phet-io/john-travoltage-phet-io-elements-baseline.js
+++ js/phet-io/john-travoltage-phet-io-elements-baseline.js
@@ -467,7 +467,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "PhetioSingletonIO<DialogIO>"
+      "phetioTypeName": "PhetioCapsuleIO<DialogIO>"
     },
     "johnTravoltage.general.navigationBar.a11yButtonsHBox.keyboardHelpButton.keyboardHelpDialogSingleton.prototype": {
       "phetioDocumentation": "",
@@ -766,7 +766,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "PhetioSingletonIO<DialogIO>"
+      "phetioTypeName": "PhetioCapsuleIO<DialogIO>"
     },
     "johnTravoltage.general.navigationBar.phetButton.phetMenu.aboutDialogSingleton.prototype": {
       "phetioDocumentation": "",
