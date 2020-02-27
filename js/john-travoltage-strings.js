8d827dbd073f0a1dd52e1feb9b7eb57391f62d39
samreid <reids@colorado.edu>
2020-02-27 12:08:00 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/john-travoltage-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import johnTravoltage from './johnTravoltage.js';
+
+const johnTravoltageStrings = getStringModule( 'JOHN_TRAVOLTAGE' );
+
+johnTravoltage.register( 'johnTravoltageStrings', johnTravoltageStrings );
+
+export default johnTravoltageStrings;
