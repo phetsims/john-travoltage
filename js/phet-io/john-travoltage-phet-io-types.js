c01b32751674073a8f1d9ea9b49a141c7fb98e21
zepumph <michael.kauzmann@colorado.edu>
2020-04-01 14:29:28 -0800

    make ReferenceIO parametric, convert usages, https://github.com/phetsims/tandem/issues/159

--- js/phet-io/john-travoltage-phet-io-types.js
+++ js/phet-io/john-travoltage-phet-io-types.js
@@ -1383,7 +1383,7 @@ window.phet.phetio.phetioTypes = assert &&
       "typeName": "PropertyIO<Vector2IO>"
     },
     "ReferenceIO": {
-      "documentation": "Uses reference identity for toStateObject/fromStateObject",
+      "documentation": "Uses reference identity for toStateObject/fromStateObject and validates based on its parameter IO Type.",
       "events": [],
       "methodOrder": [],
       "methods": {},
