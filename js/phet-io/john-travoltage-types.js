5521217a9e96e9bb5d45d15a54a9bbd54d6f0bf4
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 13:46:00 -0600

    revise IO Type doc for primitive core types, https://github.com/phetsims/tandem/issues/165

--- js/phet-io/john-travoltage-types.js
+++ js/phet-io/john-travoltage-types.js
@@ -234,7 +234,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ArrayIO<StringIO>"
     },
     "BooleanIO": {
-      "documentation": "Wrapper for the built-in JS boolean type (true/false)",
+      "documentation": "IO Type for Javascript's boolean primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -713,7 +713,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "NullableIO<Vector2IO>"
     },
     "NumberIO": {
-      "documentation": "Wrapper for the built-in JS number type (floating point, but also represents integers)",
+      "documentation": "IO Type for Javascript's number primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -1426,7 +1426,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ScreenIO"
     },
     "StringIO": {
-      "documentation": "Wrapper for the built-in JS string type",
+      "documentation": "IO Type for Javascript's string primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
