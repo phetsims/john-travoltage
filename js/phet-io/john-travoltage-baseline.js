0cd4614728ee83e7fc1d05024c5cc0bedb15d8d8
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:16:28 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/john-travoltage-baseline.js
+++ js/phet-io/john-travoltage-baseline.js
@@ -2211,19 +2211,6 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "EmitterIO<StringIO>"
     },
-    "johnTravoltage.johnTravoltageScreen.nameProperty": {
-      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
-      "phetioDynamicElement": false,
-      "phetioEventType": "MODEL",
-      "phetioFeatured": true,
-      "phetioHighFrequency": false,
-      "phetioIsArchetype": false,
-      "phetioPlayback": false,
-      "phetioReadOnly": false,
-      "phetioState": true,
-      "phetioStudioControl": true,
-      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
-    },
     "johnTravoltage.johnTravoltageScreen.view": {
       "phetioDocumentation": "",
       "phetioDynamicElement": false,
