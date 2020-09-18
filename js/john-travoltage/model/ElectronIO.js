a3d35e5e5fcd6a13d0595d23be818cf92e6025c3
samreid <reids@colorado.edu>
2020-09-17 20:59:28 -0600

    Convert to IOType, see https://github.com/phetsims/tandem/issues/211

--- js/john-travoltage/model/ElectronIO.js
+++ js/john-travoltage/model/ElectronIO.js
@@ -7,12 +7,12 @@
  * @author Andrew Adare (PhET Interactive Simulations)
  */
 
-import validate from '../../../../axon/js/validate.js';
-import ObjectIO from '../../../../tandem/js/types/ObjectIO.js';
+import IOType from '../../../../tandem/js/types/IOType.js';
 import johnTravoltage from '../../johnTravoltage.js';
 
-class ElectronIO extends ObjectIO {
-
+const ElectronIO = new IOType( 'ElectronIO', {
+  isValidValue: v => v instanceof phet.johnTravoltage.Electron,
+  documentation: 'Electron in John\'s body',
 
   /**
    * @public
@@ -20,22 +20,20 @@ class ElectronIO extends ObjectIO {
    * @returns {Object}
    * @override
    */
-  static toStateObject( electron ) {
-    validate( electron, this.validator );
+  toStateObject( electron ) {
     return {
       history: electron.history,
       velocityX: electron.velocity.x,
       velocityY: electron.velocity.y
     };
-  }
+  },
 
   /**
    * @public
    * @param {Electron} electron
    * @param {Object} stateObject
    */
-  static applyState( electron, stateObject ) {
-    validate( electron, this.validator );
+  applyState( electron, stateObject ) {
     assert && assert( stateObject.history, 'value should have history' );
     electron.history = stateObject.history;
     electron.velocity.x = stateObject.velocityX;
@@ -44,12 +42,7 @@ class ElectronIO extends ObjectIO {
     // Trigger a computation of screen position
     electron.historyChangedEmitter.emit();
   }
-}
-
-ElectronIO.validator = { isValidValue: v => v instanceof phet.johnTravoltage.Electron };
-ElectronIO.documentation = 'Electron in John\'s body';
-ElectronIO.typeName = 'ElectronIO';
-ObjectIO.validateIOType( ElectronIO );
+} );
 
 johnTravoltage.register( 'ElectronIO', ElectronIO );
 export default ElectronIO;
