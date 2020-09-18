cf56c876f7751e0e8450e150206eada6d0e564e9
samreid <reids@colorado.edu>
2020-09-17 21:41:54 -0600

    Use arrow functions in IOType state functions, see https://github.com/phetsims/tandem/issues/211

--- js/john-travoltage/model/ElectronIO.js
+++ js/john-travoltage/model/ElectronIO.js
@@ -13,27 +13,12 @@ import johnTravoltage from '../../johnTravoltage.js';
 const ElectronIO = new IOType( 'ElectronIO', {
   isValidValue: v => v instanceof phet.johnTravoltage.Electron,
   documentation: 'Electron in John\'s body',
-
-  /**
-   * @public
-   * @param {Electron} electron
-   * @returns {Object}
-   * @override
-   */
-  toStateObject( electron ) {
-    return {
-      history: electron.history,
-      velocityX: electron.velocity.x,
-      velocityY: electron.velocity.y
-    };
-  },
-
-  /**
-   * @public
-   * @param {Electron} electron
-   * @param {Object} stateObject
-   */
-  applyState( electron, stateObject ) {
+  toStateObject: electron => ( {
+    history: electron.history,
+    velocityX: electron.velocity.x,
+    velocityY: electron.velocity.y
+  } ),
+  applyState: ( electron, stateObject ) => {
     assert && assert( stateObject.history, 'value should have history' );
     electron.history = stateObject.history;
     electron.velocity.x = stateObject.velocityX;
