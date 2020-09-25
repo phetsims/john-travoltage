4dc5a4e4f501a2aaf91935fe38142ae49446a8c0
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:40:10 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/john-travoltage/JohnTravoltageScreen.js
+++ js/john-travoltage/JohnTravoltageScreen.js
@@ -8,30 +8,29 @@
 
 import Property from '../../../axon/js/Property.js';
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import johnTravoltage from '../johnTravoltage.js';
 import JohnTravoltageModel from './model/JohnTravoltageModel.js';
 import JohnTravoltageView from './view/JohnTravoltageView.js';
 
-/**
- * @param tandem
- * @constructor
- */
-function JohnTravoltageScreen( tandem ) {
-  Screen.call( this,
-    function() {
-      return new JohnTravoltageModel( tandem.createTandem( 'model' ) );
-    },
-    function( model ) {
-      return new JohnTravoltageView( model, tandem.createTandem( 'view' ) );
-    }, {
-      backgroundColorProperty: new Property( '#9ddcf8' ),
-      tandem: tandem
-    }
-  );
+class JohnTravoltageScreen extends Screen {
+
+  /**
+   * @param {Tandem} tandem
+   */
+  constructor( tandem ) {
+    super(
+      function() {
+        return new JohnTravoltageModel( tandem.createTandem( 'model' ) );
+      },
+      function( model ) {
+        return new JohnTravoltageView( model, tandem.createTandem( 'view' ) );
+      }, {
+        backgroundColorProperty: new Property( '#9ddcf8' ),
+        tandem: tandem
+      }
+    );
+  }
 }
 
 johnTravoltage.register( 'JohnTravoltageScreen', JohnTravoltageScreen );
-
-inherit( Screen, JohnTravoltageScreen );
 export default JohnTravoltageScreen;
