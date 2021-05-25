dff1f9d5198503bdea9caf45b01136bd3e8b1d15
phet-dev <phetadmin@gmail.com>
2021-05-25 15:53:20 -0600

    update copyright dates from daily grunt work

--- js/john-travoltage/view/FootDragSoundGenerator.js
+++ js/john-travoltage/view/FootDragSoundGenerator.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * Sound generator used to produce a sound like that of a foot being dragged on a carpet.
