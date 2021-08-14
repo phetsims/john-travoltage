597a1df1f23d04aaee6452416e617d9b8395c062
Jesse <jesse.greenberg@colorado.edu>
2021-08-14 13:42:27 -0400

    update credits, see https://github.com/phetsims/john-travoltage/issues/446

--- js/john-travoltage-main.js
+++ js/john-travoltage-main.js
@@ -33,7 +33,7 @@ const simOptions = {
     leadDesign: 'Noah Podolefsky, Carl Wieman, Sam Reid',
     softwareDevelopment: 'Sam Reid, John Blanco',
     team: 'Wendy Adams, Jesse Greenberg, Trish Loeblein, Emily B. Moore, Ariel Paul, Kathy Perkins,<br>Taliesin Smith',
-    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ben Roberts, Oliver Orejola, Kathryn Woessner',
+    qualityAssurance: 'Logan Bray, Steele Dalton, Bryce Griebenow, Clifford Hardin, Liam Mulhall, <br>Devon Quispe, Ben Roberts, Oliver Orejola, Nancy Salpepi, Kathryn Woessner',
     graphicArts: 'Mariah Hermsmeyer, Sharon Siman-Tov',
     soundDesign: 'Ashton Morris, Mike Winters',
     contributors: 'Jonathan Hung, Justin Obara (Inclusive Design Research Centre)',
