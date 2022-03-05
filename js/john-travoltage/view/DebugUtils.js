8d6b39e7f7f684eca2d9d07b11d139356e7594e3
phet-dev <phetadmin@gmail.com>
2022-03-05 03:25:15 -0700

    update copyright dates from daily grunt work

--- js/john-travoltage/view/DebugUtils.js
+++ js/john-travoltage/view/DebugUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Utility for sampling positions and line segments for the model, which should be used in the JohnTravoltageView.
