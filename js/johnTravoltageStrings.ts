2c7c04e0b739249e9cd78674c05e699ecfdd406b
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/johnTravoltageStrings.ts
+++ js/johnTravoltageStrings.ts
@@ -5,82 +5,136 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import johnTravoltage from './johnTravoltage.js';
 
 type StringsType = {
   'john-travoltage': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'or': string;
+  'orProperty': TReadOnlyProperty<string>;
   'shiftKey': string;
+  'shiftKeyProperty': TReadOnlyProperty<string>;
   'escKey': string;
+  'escKeyProperty': TReadOnlyProperty<string>;
   'tabKey': string;
+  'tabKeyProperty': TReadOnlyProperty<string>;
   'handOrFoot': string;
+  'handOrFootProperty': TReadOnlyProperty<string>;
   'moveHandOrFoot': string;
+  'moveHandOrFootProperty': TReadOnlyProperty<string>;
   'moveFoot': string;
+  'moveFootProperty': TReadOnlyProperty<string>;
   'moveHand': string;
+  'moveHandProperty': TReadOnlyProperty<string>;
   'hotKeysAndHelp': string;
+  'hotKeysAndHelpProperty': TReadOnlyProperty<string>;
   'arrowKeysMoveFoot': string;
+  'arrowKeysMoveFootProperty': TReadOnlyProperty<string>;
   'tabKeyDescription': string;
+  'tabKeyDescriptionProperty': TReadOnlyProperty<string>;
   'shiftTabKeyDescription': string;
+  'shiftTabKeyDescriptionProperty': TReadOnlyProperty<string>;
   'escapeKeyDescription': string;
+  'escapeKeyDescriptionProperty': TReadOnlyProperty<string>;
   'a11y': {
     'screenSummary': {
       'bodyDescriptionPattern': string;
+      'bodyDescriptionPatternProperty': TReadOnlyProperty<string>;
       'descriptionWithChargePattern': string;
+      'descriptionWithChargePatternProperty': TReadOnlyProperty<string>;
     };
     'electrons': {
       'singleDescription': string;
+      'singleDescriptionProperty': TReadOnlyProperty<string>;
       'multipleDescriptionPattern': string;
+      'multipleDescriptionPatternProperty': TReadOnlyProperty<string>;
       'totalDescriptionPattern': string;
+      'totalDescriptionPatternProperty': TReadOnlyProperty<string>;
       'totalAfterDischargePattern': string;
+      'totalAfterDischargePatternProperty': TReadOnlyProperty<string>;
     };
     'appendages': {
       'arm': {
         'label': string;
+        'labelProperty': TReadOnlyProperty<string>;
         'positions': {
           'farthestFromDoorknob': string;
+          'farthestFromDoorknobProperty': TReadOnlyProperty<string>;
           'veryFarFromDoorknob': string;
+          'veryFarFromDoorknobProperty': TReadOnlyProperty<string>;
           'farFromDoorknob': string;
+          'farFromDoorknobProperty': TReadOnlyProperty<string>;
           'notSoCloseToDoorknob': string;
+          'notSoCloseToDoorknobProperty': TReadOnlyProperty<string>;
           'closeToDoorknob': string;
+          'closeToDoorknobProperty': TReadOnlyProperty<string>;
           'veryCloseToDoorknob': string;
+          'veryCloseToDoorknobProperty': TReadOnlyProperty<string>;
           'justAboveDoorknob': string;
+          'justAboveDoorknobProperty': TReadOnlyProperty<string>;
           'atDoorknob': string;
+          'atDoorknobProperty': TReadOnlyProperty<string>;
           'justBelowDoorknob': string;
+          'justBelowDoorknobProperty': TReadOnlyProperty<string>;
           'handPointingAway': string;
+          'handPointingAwayProperty': TReadOnlyProperty<string>;
           'handPointingStraightUp': string;
+          'handPointingStraightUpProperty': TReadOnlyProperty<string>;
           'handPointingAtUpperDoor': string;
+          'handPointingAtUpperDoorProperty': TReadOnlyProperty<string>;
           'handPointingAtLowerDoor': string;
+          'handPointingAtLowerDoorProperty': TReadOnlyProperty<string>;
           'handPointingStraightDown': string;
+          'handPointingStraightDownProperty': TReadOnlyProperty<string>;
           'farthestFromDoorknobLast': string;
+          'farthestFromDoorknobLastProperty': TReadOnlyProperty<string>;
         }
       };
       'leg': {
         'label': string;
+        'labelProperty': TReadOnlyProperty<string>;
         'positions': {
           'footOffRugPointingBackwards': string;
+          'footOffRugPointingBackwardsProperty': TReadOnlyProperty<string>;
           'footOffRug': string;
+          'footOffRugProperty': TReadOnlyProperty<string>;
           'footRubbingOnRug': string;
+          'footRubbingOnRugProperty': TReadOnlyProperty<string>;
           'footOffRugPointingForward': string;
+          'footOffRugPointingForwardProperty': TReadOnlyProperty<string>;
         }
       }
     };
     'keyboardHelpDialog': {
       'moveFootDescription': string;
+      'moveFootDescriptionProperty': TReadOnlyProperty<string>;
       'moveHandDescription': string;
+      'moveHandDescriptionProperty': TReadOnlyProperty<string>;
     };
     'voicing': {
       'contentHint': string;
+      'contentHintProperty': TReadOnlyProperty<string>;
       'detailedContentHint': string;
+      'detailedContentHintProperty': TReadOnlyProperty<string>;
       'chargedContentHint': string;
+      'chargedContentHintProperty': TReadOnlyProperty<string>;
       'overviewPattern': string;
+      'overviewPatternProperty': TReadOnlyProperty<string>;
       'previousDischargePattern': string;
+      'previousDischargePatternProperty': TReadOnlyProperty<string>;
       'johnFullyCharged': string;
+      'johnFullyChargedProperty': TReadOnlyProperty<string>;
       'multipleElectronsOnBodyPattern': string;
+      'multipleElectronsOnBodyPatternProperty': TReadOnlyProperty<string>;
       'screenSummaryWithPreviousDischargePattern': string;
+      'screenSummaryWithPreviousDischargePatternProperty': TReadOnlyProperty<string>;
       'handInteractionHint': string;
+      'handInteractionHintProperty': TReadOnlyProperty<string>;
       'footInteractionHint': string;
+      'footInteractionHintProperty': TReadOnlyProperty<string>;
     }
   }
 };
